858d0c5fd7d9fc56a8240b247316d5e1b13a5d27
Sai Rahul Urumu <123154452+usrahul1@users.noreply.github.com>
2024-06-07 16:04:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,80 +25,22 @@ props.forEach(prop => {
     });
 });
 
-buttons.forEach(button => {
-    button.addEventListener('click', (event) =>{
-        if(ops === undefined){
-            if(opr1 === undefined){
-                opr1 = "";
-            }
-            opr1 = String(opr1)
-            opr1 += event.target.innerText;
-        }
-        else{
-            if(opr2 === undefined){
-                opr2 = "";
-            }
-            opr2 = String(opr2)
-            opr2 += event.target.innerText;
-        }
-    });
-})
-
-buttonsp.forEach(buttonsps  =>{
-    buttonsps.addEventListener('click', (event)=>{
-        if(ops ===undefined){
-            ops = event.target.innerText;
-        }
-    })
-})
-
 let y = document.getElementById("end");
 
 y.addEventListener('click', (event)=>{
-    if(ops!==undefined && opr1!==undefined && opr2!==undefined){
-        
-        switch(ops){
-            case '+': ops = add; break;
-            case '-': ops = sub; break;
-            case '*': ops = mul; break;
-            case '/': ops = div; break;
-            default: alert("Please enter properly"); break;
-        }
-
-        final = calc(Number(opr1), Number(opr2), ops)
-        ops=opr2="";
-        opr1=final;
-        op.innerText = final;
-        ops=undefined;
-    }
-    else{
-        alert("Please enter properly");
-    }
+    op.innerText = eval(op.innerText);
 })
 
-function calc(opr1, opr2, ops){
-    return ops(opr1, opr2);
-}
-
-function add(opr1, opr2){
-    return opr1+opr2;
-}
-
-function sub(opr1, opr2){
-    return opr1-opr2;
-}
-
-function mul(opr1, opr2){
-    return opr1*opr2;
-}
-
-function div(opr1, opr2){
-    return opr1/opr2;
-}
 
 let clear = document.getElementById("c");
 
 clear.addEventListener('click', ()=>{
     opr1=opr2=ops=undefined;
     op.innerText="";
+})
+
+
+let b = document.getElementById("backspace");
+b.addEventListener('click', () => {
+    op.innerText = op.innerText.slice(0,-1);
 })
